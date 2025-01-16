b282174740f24368bbca8a9774a5808080491706
espaciofuturoio <dev@espaciofuturo.io>
2025-01-15 20:50:19 -0600

    chore: add husky

--- features/room/canvas/realtime/useStudyRoom.ts
+++ features/room/canvas/realtime/useStudyRoom.ts
@@ -1,83 +1,102 @@
-import { useEffect } from "react";
-import type { PlayerLayout, StudyRoomState, TileLayout } from "./StudyRoomState.schema";
 import { useColyseus } from "@/libs/use-colyseus";
+import { useEffect } from "react";
+import type {
+  PlayerLayout,
+  StudyRoomState,
+  TileLayout,
+} from "./StudyRoomState.schema";
 
 // const serverWS = 'ws://localhost:2567'
-const serverWS = 'wss://dev-rooms-game-server-631092729836.us-central1.run.app'
+const serverWS = "wss://dev-rooms-game-server-631092729836.us-central1.run.app";
 
 const convertTo2DArray = (
-	tiles: TileLayout[] | undefined,
-	widthUnits: number | undefined,
-	heightUnits: number | undefined,
+  tiles: TileLayout[] | undefined,
+  widthUnits: number | undefined,
+  heightUnits: number | undefined,
 ): TileLayout[][] => {
-	if (!tiles || !widthUnits || !heightUnits) return [];
-	const layout: TileLayout[][] = [];
+  if (!tiles || !widthUnits || !heightUnits) return [];
+  const layout: TileLayout[][] = [];
 
-	for (let y = 0; y < heightUnits; y++) {
-		layout[y] = [];
-		for (let x = 0; x < widthUnits; x++) {
-			const index = y * widthUnits + x;
-			layout[y][x] = tiles[index];
-		}
-	}
+  for (let y = 0; y < heightUnits; y++) {
+    layout[y] = [];
+    for (let x = 0; x < widthUnits; x++) {
+      const index = y * widthUnits + x;
+      layout[y][x] = tiles[index];
+    }
+  }
 
-	return layout;
+  return layout;
 };
 
 const toMap = <T>(tiles: Record<string, T>): Array<[string, T]> => {
-	if (!tiles) return [];
-	return Object.entries(tiles);
+  if (!tiles) return [];
+  return Object.entries(tiles);
 };
 
 export const useStudyRoom = () => {
-
-const {
+  const {
     connectToColyseus,
     disconnectFromColyseus,
     useColyseusRoom,
     useColyseusState,
-    sendMessage
-  } = useColyseus<StudyRoomState>(serverWS)
+    sendMessage,
+  } = useColyseus<StudyRoomState>(serverWS);
 
-	useEffect(() => {
-		(async () => {
-			await connectToColyseus("study-room");
-			console.log("connected to colyseus");
-		})();
+  // biome-ignore lint/correctness/useExhaustiveDependencies: only run once
+  useEffect(() => {
+    (async () => {
+      await connectToColyseus("study-room");
+      console.log("connected to colyseus!");
+    })();
 
-		return () => {
-			console.log("disconnecting from colyseus");
-			disconnectFromColyseus();
-		};
-	}, []);
+    return () => {
+      console.log("disconnecting from colyseus!");
+      disconnectFromColyseus();
+    };
+  }, []);
 
-	// TODO: improve reactivity to avoid re-render
-	// Players are not reactive???
-	const state = useColyseusState();
+  // TODO: improve reactivity to avoid re-render
+  // Players are not reactive???
+  const state = useColyseusState();
 
-	const sessionId = useColyseusRoom()?.sessionId;
-	const widthUnits = useColyseusState((state) => state.widthUnits);
-	const heightUnits = useColyseusState((state) => state.heightUnits);
-	const layout1D = useColyseusState(
-		(state) => state.layout,
-	)?.toJSON() as TileLayout[];
-	const layout2D = convertTo2DArray(layout1D, widthUnits, heightUnits);
-	const players = toMap<PlayerLayout>(useColyseusState(
-		(state) => state.players,
-	)?.toJSON());
+  const sessionId = useColyseusRoom()?.sessionId;
+  const widthUnits = useColyseusState((state) => state.widthUnits);
+  const heightUnits = useColyseusState((state) => state.heightUnits);
+  const layout1D = useColyseusState(
+    (state) => state.layout,
+  )?.toJSON() as TileLayout[];
+  const layout2D = convertTo2DArray(layout1D, widthUnits, heightUnits);
+  const players = toMap<PlayerLayout>(
+    useColyseusState((state) => state.players)?.toJSON(),
+  );
 
+  const movePlayer = (dx: number, dy: number, direction: string) => {
+    console.log("movePlayer", dx, dy, direction);
+    sendMessage("move", { dx, dy, direction });
+  };
 
-	const movePlayer = (dx: number, dy: number, direction: string) => {
-		console.log("movePlayer", dx, dy, direction);
-		sendMessage("move", { dx, dy, direction });
-	};
+  const stopPlayer = (key: string) => {
+    console.log("stopPlayer", key);
+    sendMessage("stop", { key });
+  };
 
-	const stopPlayer = (key: string) => {
-		console.log("stopPlayer", key);
-		sendMessage("stop", { key });
-	};
+  console.log(
+    "players ---->",
+    players.map(([id, player]) => ({
+      playerId: id,
+      action: player.action,
+      direction: player.direction,
+    })),
+  );
 
-	console.log("players ---->", players.map(([id, player]) => ({playerId: id, action: player.action, direction: player.direction})));
-
-	return { sessionId, widthUnits, heightUnits, layout: layout2D, players, movePlayer, stopPlayer, state };
+  return {
+    sessionId,
+    widthUnits,
+    heightUnits,
+    layout: layout2D,
+    players,
+    movePlayer,
+    stopPlayer,
+    state,
+  };
 };
